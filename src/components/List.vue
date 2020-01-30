b730e8058d72bc0302a44a80af9da8a377f9e772
王澍(Andrew) <andrewwang1@micb2b.com>
2020-01-30 17:20:37 +0800

    add input text field

--- src/components/List.vue
+++ src/components/List.vue
@@ -3,8 +3,16 @@
         <div class="single" v-bind:key="item" v-for="item in list">
             <div class="circle"></div>
             <div class="text">{{item}}</div>
-            <img src="../assets/play.png">
+            <img src="../assets/play.png" height="50">
         </div>
+        <div class="container">
+            <input class="input" type="text" placeholder="Add A New Mission..."/>
+
+            <div class="plus">
+                <img src="../assets/plus.png" height="40">
+            </div>
+        </div>
+
     </div>
 </template>
 
@@ -49,6 +57,40 @@
         padding-right: 20px;
         display: flex;
         flex: 1;
+    }
+
+    .input {
+        border: 3px solid #d8564a;
+        border-radius: 3px;
+        display: flex;
+        height: 50px;
+        width: 700px;
+        flex: 1;
+        color: #df564a;
+        font-size: 30px;
+        padding: 5px 15px 5px 15px; /* top right bottom left */
+    }
+
+    .input::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
+        color: #df564a;
+        opacity: 1; /* Firefox */
+    }
+
+    .container {
+        position: relative;
+        align-items: center;
+    }
+
+    .plus {
+        position: absolute;
+        right: 0;
+        top: 0;
+        height: 100%;
+        width: 60px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-left: 3px solid #d8564a;
     }
 
 
